8c696709747d060db4494c624bb97caea5fd33bd
arifusr <arifusr@gmail.com>
2020-10-05 07:05:01 +0700

    eslint

--- src/app.js
+++ src/app.js
@@ -1,50 +1,18 @@
-'use strict';
+const express = require('express')
 
-const express = require('express');
-const app = express();
+const app = express()
 
-const bodyParser = require('body-parser');
-const jsonParser = bodyParser.json();
-const { checkSchema, validationResult } = require('express-validator');
+const bodyParser = require('body-parser')
 
-const validator = (req, res, next)=>{
-    const errors = validationResult(req);
-    if (!errors.isEmpty()) {
-      return res.send(errors.array()[0].msg);
-    }
-    next()
-}
+const jsonParser = bodyParser.json()
+const Validator = require('jsonschema').Validator;
+const validator = new Validator();
+const ReqRiderSchema = require('../.schema/rides.req.json')
 
-const isExist = (name) => ({
-    options: (value, context) => {
-        if(value === undefined || value === null) {
-            throw {
-            error_code: 'VALIDATION_ERROR',
-            message: `${name} must be provided`}
-        }
-        return true;
-      
-    }
-  })
 
-const numbernotempty = (name)=> ({
-    in: 'body',
-    custom: isExist(name),
-    isInt: {
-        errorMessage: {
-            error_code: 'VALIDATION_ERROR',
-            message: `${name} must be integer`
-        }
-    }
-})
-
-const stringnotempty = (name)=> ({
-    in: 'body',
-    custom: isExist(name),
-})
 
 module.exports = (db) => {
-    /**
+  /**
      * @api {get} /health Request health information
      * @apiName GetHealth
      * @apiGroup Health
@@ -54,13 +22,13 @@ module.exports = (db) => {
      * @apiSuccessExample {String} Success-Response:
      *     HTTP/1.1 200 OK
      *     Healthy
-     * 
+     *
      * @apiExample {curl} Example usage:
      *      curl -i http://localhost:8010/health
      */
-    app.get('/health', (req, res) => res.send('Healthy'));
+  app.get('/health', (req, res) => res.send('Healthy'))
 
-    /**
+  /**
      * @api {post} /rides Create new ride information
      * @apiName PostRides
      * @apiGroup Rides
@@ -96,124 +64,83 @@ module.exports = (db) => {
      *         "created": "2020-09-28 04:22:50"
      *      }
      *     ]
-     * 
+     *
      * @apiError UserNotFound The <code>id</code> of the User was not found
      */
-    app.post('/rides', jsonParser, checkSchema({
-        start_lat: numbernotempty('start_lat'),
-        start_long: numbernotempty('start_long'),
-        end_lat: numbernotempty('end_lat'),
-        end_long: numbernotempty('end_long'),
-        rider_name: stringnotempty('rider_name'),   
-        driver_name: stringnotempty('driver_name'),
-        driver_vehicle: stringnotempty('driver_name')
-        
-      }), validator, (req, res) => {
-        const startLatitude = Number(req.body.start_lat);
-        const startLongitude = Number(req.body.start_long);
-        const endLatitude = Number(req.body.end_lat);
-        const endLongitude = Number(req.body.end_long);
-        const riderName = req.body.rider_name;
-        const driverName = req.body.driver_name;
-        const driverVehicle = req.body.driver_vehicle;
+  app.post('/rides', jsonParser, (req, res, next) => {
+    const { errors } = validator.validate(req.body, ReqRiderSchema)
+    if(errors.length) {
+      const errorMessage = errors.reduce((t,v)=>{return t+`${v.property.replace('instance.','')} ${v.message}; `},"")
+      console.log(errorMessage)
+      return res.status(400).json({
+        error_code: 'SERVER_ERROR',
+        message: errorMessage
+      })
+    }
+    return next()
+  }, (req, res) => {
+    const values = [req.body.start_lat, req.body.start_long, req.body.end_lat, req.body.end_long, req.body.rider_name, req.body.driver_name, req.body.driver_vehicle]
 
-        if (startLatitude < -90 || startLatitude > 90 || startLongitude < -180 || startLongitude > 180) {
-            return res.send({
-                error_code: 'VALIDATION_ERROR',
-                message: 'Start latitude and longitude must be between -90 - 90 and -180 to 180 degrees respectively'
-            });
+    db.run('INSERT INTO Rides(startLat, startLong, endLat, endLong, riderName, driverName, driverVehicle) VALUES (?, ?, ?, ?, ?, ?, ?)', values, function (err) {
+      if (err) {
+        return res.send({
+          error_code: 'SERVER_ERROR',
+          message: 'Unknown error'
+        })
+      }
+
+      db.all('SELECT * FROM Rides WHERE rideID = ?', this.lastID, (err, rows) => {
+        if (err) {
+          return res.send({
+            error_code: 'SERVER_ERROR',
+            message: 'Unknown error'
+          })
         }
 
-        if (endLatitude < -90 || endLatitude > 90 || endLongitude < -180 || endLongitude > 180) {
-            return res.send({
-                error_code: 'VALIDATION_ERROR',
-                message: 'End latitude and longitude must be between -90 - 90 and -180 to 180 degrees respectively'
-            });
-        }
+        res.send(rows)
+      })
+    })
+  })
 
-        if (typeof riderName !== 'string' || riderName.length < 1) {
-            return res.send({
-                error_code: 'VALIDATION_ERROR',
-                message: 'Rider name must be a non empty string'
-            });
-        }
+  app.get('/rides', (req, res) => {
+    db.all('SELECT * FROM Rides', (err, rows) => {
+      if (err) {
+        return res.send({
+          error_code: 'SERVER_ERROR',
+          message: 'Unknown error'
+        })
+      }
 
-        if (typeof driverName !== 'string' || driverName.length < 1) {
-            return res.send({
-                error_code: 'VALIDATION_ERROR',
-                message: 'Driver name must be a non empty string'
-            });
-        }
+      if (rows.length === 0) {
+        return res.send({
+          error_code: 'RIDES_NOT_FOUND_ERROR',
+          message: 'Could not find any rides'
+        })
+      }
 
-        if (typeof driverVehicle !== 'string' || driverVehicle.length < 1) {
-            return res.send({
-                error_code: 'VALIDATION_ERROR',
-                message: 'Driver vehicle must be a non empty string'
-            });
-        }
+      res.send(rows)
+    })
+  })
 
-        var values = [req.body.start_lat, req.body.start_long, req.body.end_lat, req.body.end_long, req.body.rider_name, req.body.driver_name, req.body.driver_vehicle];
-        
-        const result = db.run('INSERT INTO Rides(startLat, startLong, endLat, endLong, riderName, driverName, driverVehicle) VALUES (?, ?, ?, ?, ?, ?, ?)', values, function (err) {
-            if (err) {
-                return res.send({
-                    error_code: 'SERVER_ERROR',
-                    message: 'Unknown error'
-                });
-            }
+  app.get('/rides/:id', (req, res) => {
+    db.all(`SELECT * FROM Rides WHERE rideID='${req.params.id}'`, (err, rows) => {
+      if (err) {
+        return res.send({
+          error_code: 'SERVER_ERROR',
+          message: 'Unknown error'
+        })
+      }
 
-            db.all('SELECT * FROM Rides WHERE rideID = ?', this.lastID, function (err, rows) {
-                if (err) {
-                    return res.send({
-                        error_code: 'SERVER_ERROR',
-                        message: 'Unknown error'
-                    });
-                }
+      if (rows.length === 0) {
+        return res.send({
+          error_code: 'RIDES_NOT_FOUND_ERROR',
+          message: 'Could not find any rides'
+        })
+      }
 
-                res.send(rows);
-            });
-        });
-    });
+      res.send(rows)
+    })
+  })
 
-    app.get('/rides', (req, res) => {
-        db.all('SELECT * FROM Rides', function (err, rows) {
-            if (err) {
-                return res.send({
-                    error_code: 'SERVER_ERROR',
-                    message: 'Unknown error'
-                });
-            }
-
-            if (rows.length === 0) {
-                return res.send({
-                    error_code: 'RIDES_NOT_FOUND_ERROR',
-                    message: 'Could not find any rides'
-                });
-            }
-
-            res.send(rows);
-        });
-    });
-
-    app.get('/rides/:id', (req, res) => {
-        db.all(`SELECT * FROM Rides WHERE rideID='${req.params.id}'`, function (err, rows) {
-            if (err) {
-                return res.send({
-                    error_code: 'SERVER_ERROR',
-                    message: 'Unknown error'
-                });
-            }
-
-            if (rows.length === 0) {
-                return res.send({
-                    error_code: 'RIDES_NOT_FOUND_ERROR',
-                    message: 'Could not find any rides'
-                });
-            }
-
-            res.send(rows);
-        });
-    });
-
-    return app;
-};
+  return app
+}
