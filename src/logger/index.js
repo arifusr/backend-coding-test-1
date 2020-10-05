bc2145d477fade23280a3de5f1121d62d3ff5bd8
arifusr <arifusr@gmail.com>
2020-10-05 07:28:53 +0700

    winston logger

---
+++ src/logger/index.js
@@ -0,0 +1,15 @@
+const winston = require('winston');
+
+module.exports = winston.createLogger({
+  format: winston.format.json(),
+  transports: [
+    //
+    // - Write all logs with level `error` and below to `error.log`
+    // - Write all logs with level `info` and below to `combined.log`
+    //
+    new winston.transports.File({ filename: 'log/error.log', level: 'error' }),
+    new winston.transports.File({ filename: 'log/combined.log' }),
+  ],
+});
+
+
